46a84d42ba01e1079fd8c254de05fab5e7867684
Rayyan Khalique <rayyankhalique@gmail.com>
2025-11-21 12:56:38 -0800

    added character and started character animations

--- HighRiseRunner/Assets/Scripts/PlayerMovement.cs
+++ HighRiseRunner/Assets/Scripts/PlayerMovement.cs
@@ -6,6 +6,9 @@ public class PlayerMovement : MonoBehaviour
     public float playerSpeed = 2;
     public float horizontalSpeed = 3;
 
+    public float rightLimit = 7;
+    public float leftLimit = -7;
+
     InputAction moveAction;
 
     private void Start()
@@ -22,5 +25,9 @@ public class PlayerMovement : MonoBehaviour
         Vector3 translation = Vector3.forward * moveInput.y * Time.deltaTime * playerSpeed + Vector3.right * moveInput.x * Time.deltaTime * horizontalSpeed;
 
         transform.Translate(translation);
+
+        Vector3 pos = transform.position;
+        pos.x = Mathf.Clamp(pos.x, leftLimit, rightLimit);
+        transform.position = pos;
     }
 }
